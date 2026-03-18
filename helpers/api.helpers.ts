b39ca3b5634307440a31e6a80643d8c117367331
Antonina H <justantonia17@gmail.com>
2026-03-18 15:36:09 +0100

    Beautify2

--- helpers/api.helpers.ts
+++
@@ -1,6 +0,0 @@
-// Superseded. API helpers have been split and moved to:
-//   helpers/api-helpers/auth.helper.ts        → loginAPI()
-//   helpers/api-helpers/corporation.helper.ts → createCorporationAPI(), deleteCorporationAPI()
-//
-// This file is kept only because it cannot be deleted programmatically.
-// Do not add new code here.
